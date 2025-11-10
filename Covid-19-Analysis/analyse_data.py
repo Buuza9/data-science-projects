4a3ed656f57a12fb7475c681d9c4751a64c25ce1
Mohammed Salah <buuza.salah99@gmail.com>
2025-11-10 10:54:09 +0200

    Im this projectm we will analyse a set of Covid-19 data and will visualize it by using Plotly Express in Python, This projects deals with making/creating a dozen of charts, graphs, line graphs, bubble charts, and scatter plots. The graphs are made with high quality. Envisioning COVI D-19 will primarily be using Plotly Express. The main scope of this project is to make analysis and visualizations that enahance people understanding of complex scenarios make studiese and predections about the future based on the these data on the current situation.

---
+++ Covid-19-Analysis/analyse_data.py
@@ -0,0 +1,11 @@
+# Data analysis and manipulation
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.io as pio
+import plotly.express as px
+
+# Data visualization
+import matplotlib.pyplot as plt
+
+# Import plotly
+import plotly.offline as py
